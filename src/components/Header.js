8b64af2debe14c84b05fbf10913c53543d2ca48f
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-08-15 16:42:50 +0300

    <Running Tests in React>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Header(props) {
-      return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
+function Header() {
+  return (
+    <h1>
+      hello from the Header!
+    </h1>
+  );
 }
 
 export default Header;
+
